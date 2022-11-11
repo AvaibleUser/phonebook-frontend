e19817973fdc6d739b57c12af41b979b862c857a
AvaibleUser <100001088+AvaibleUser@users.noreply.github.com>
2022-11-10 22:08:39 -0600

    Components refactored, filter option added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
 
-const Person = ({ name, number }) => (
-  <p>
-    {name} {number}
-  </p>
-);
-
-const Input = ({ label, value, setValue }) => {
-  const catchValueChange = ({ target: { value } }) => {
-    setValue(value);
-  };
-
-  return (
-    <div>
-      {label}: <input value={value} onChange={catchValueChange} />
-    </div>
-  );
-};
+import Persons from './components/Persons';
+import PersonForm from './components/PersonForm';
+import PersonsFilter from './components/PersonsFilter';
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -31,46 +17,31 @@ const App = () => {
   const [filter, setFilter] = useState('');
   const [filteredPersons, setFilteredPersons] = useState(persons);
 
-  const addPerson = (event) => {
-    event.preventDefault();
-    if (!persons.find(({ number }) => number === newNumber)) {
-      setPersons(persons.concat({ name: newName, number: newNumber }));
-      setNewName('');
-      setNewNumber('');
-    } else {
-      alert(`${newName} is already added to phonebook`);
-    }
-  };
-
-  const filterPersons = (name) => {
-    const nameRegex = new RegExp(name, 'i');
-    const newFilteredPersons = persons.filter((person) =>
-      nameRegex.test(person.name)
-    );
-
-    setFilteredPersons(newFilteredPersons);
-    setFilter(name);
+  const _setPersons = (persons) => {
+    setFilter('');
+    setPersons(persons);
+    setFilteredPersons(persons);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-        <Input label="filter shown with" value={filter} setValue={filterPersons} />
-      </form>
-      <form>
-        <Input label="name" value={newName} setValue={setNewName} />
-        <Input label="number" value={newNumber} setValue={setNewNumber} />
-        <div>
-          <button type="submit" onClick={addPerson}>
-            add
-          </button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {filteredPersons.map((person) => (
-        <Person {...person} key={person.name} />
-      ))}
+      <PersonsFilter {...{ filter, setFilter, persons, setFilteredPersons }} />
+
+      <h3>Add a new</h3>
+      <PersonForm
+        {...{
+          newName,
+          newNumber,
+          persons,
+          setNewName,
+          setNewNumber,
+          setPersons: _setPersons,
+        }}
+      />
+
+      <h3>Numbers</h3>
+      <Persons persons={filteredPersons} />
     </div>
   );
 };
