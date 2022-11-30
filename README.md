bd211b38a0af078483d75221e0f7a2d22602f173
Dylan <100001088+AvaibleUser@users.noreply.github.com>
2022-11-30 22:09:47 +0000

    Change hardcoded phonebook for fetching to server
    Before the initial state of the phonebook was hardcoded in App.js, now
    the data will be fetched from a server (json-server with db.json).
    
    Added to the funtionality it was added files to build docker images
    with nginx to can run the app in codespaces.

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# The-Phonebook_FullstackOpen
+# The Phonebook - A FullstackOpen Project
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-1e63fd)
+A phonebook that holds the a bunch of person name and his phone number,
+made with react
+
+## How to run it
+
+### PC environment
+
+You need to have installed node and npm, and the ports 3000 and 3001 free, then:
+
+1. open a terminal
+2. run `$ npm install`
+3. run `$ npm run server`
+4. open another terminal
+5. run `$ npm run start`
+
+Now you can go to [phonebook](localhost:3000) and see the website
+
+### Codespaces environment
+
+1. open a terminal
+2. run `$ npm run docker`
+
+Now you can go to look in the ports forwarded for the link to the website
--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
 
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import PersonsFilter from './components/PersonsFilter';
 
+const url = '/api/persons';
+
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
@@ -22,6 +21,14 @@ const App = () => {
     setPersons(persons);
     setFilteredPersons(persons);
   };
+
+  useEffect(() => {
+    const setPersonsFromServer = (res) => _setPersons(res.data);
+
+    axios
+      .get(url)
+      .then(setPersonsFromServer);
+  }, []);
 
   return (
     <div>
